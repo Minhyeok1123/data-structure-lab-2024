a939ca3c6956259b2d6872ed53f0bfed190a888e
Minhyeok1123 <kongmhhi@naver.com>
2024-04-08 12:48:40 +0900

    Rectangle

---
+++ Chap02_Array_class/Chap02_Class_Example/Rectangle.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+class Rectangle {
+private :
+	double width:
+	double weight:
+
+public :
+	Rectangle(double w, double h) {
+		width = w;
+		height = h;
+	}
+	double getArea() {
+		return width * height;
+	}
+	double getPerimeter() {
+		return 2 * (width + heiht)
+	}
+	bool isSquare() {
+		return (width == height);
+	}
+};
---
+++ Chap02_Array_class/Chap02_Class_Example/main.cpp
@@ -0,0 +1,23 @@
+//#include <cstdio>
+//#include "Car.h"
+#include Rectangle 
+/*
+int main() {
+	Car myCar(50, "K3", 4);
+	Car yourCar(100, "K5", 3);
+	myCar.display();
+	yourCar.display();
+	myCar.whereAmI();
+	yourCar.whereAmI();
+
+	SportsCar scar;
+	scar.speedUp();
+
+*/
+Rectangle r(10, 20);
+double perimeter = r.getPerimeter();
+std::cout << "Rectangle 1:" << std::endl;
+std::cout << "Area"<< r.getArea() << std::endI;
+std::cout << "Perimeter:" << perimeter << std::endI;
+std::cout << "is square?" << std::boolalpha << r.isSquare() <<std::endl:
+}
